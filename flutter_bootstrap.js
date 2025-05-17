5e92428452f0f3fb7a401f57df8cb734adddc070
ChiDuck <cheesieduck@gmail.com>
2025-05-17 10:20:20 +0700

    10

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3533974277"
+    serviceWorkerVersion: "3446868369"
   }
 });
